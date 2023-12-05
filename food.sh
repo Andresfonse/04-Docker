02d744715a253d5d447a73e69d02d1db6452aa46
deavila747 <cristiandeavila747@gmail.com>
2023-12-05 14:16:02 -0500

    04 Docker

---
+++ food.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker cp image foodrunning:/usr/share/nginx/html/
+
+docker cp creditos.html foodrunning:/usr/share/nginx/html/
+
+docker cp index.html foodrunning:/usr/share/nginx/html/
